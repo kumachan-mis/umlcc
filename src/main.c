38b7161343e3e09b59ea7f302dcd9062ed0a0cec
Kumachan <29433058+kumachan-mis@users.noreply.github.com>
2022-04-07 21:07:27 +0900

    feat: first step (#1)

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/main.h"
+
+int main() {
+    FILE* asfile = fopen("umlcc.s", "w");
+    fprintf(asfile, "    .text\n");
+    fprintf(asfile, "    .global _main\n");
+    fprintf(asfile, "_main:\n");
+    fprintf(asfile, "    pushq  %%rbp\n");
+    fprintf(asfile, "    movq  %%rsp, %%rbp\n");
+    fprintf(asfile, "    movl  $0, %%eax\n");
+    fprintf(asfile, "    popq  %%rbp\n");
+    fprintf(asfile, "    ret\n");
+    fclose(asfile);
+    return 0;
+}
