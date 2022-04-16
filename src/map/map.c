5df250e802d24c5239a8e7f0a4827c35525e2665
Kumachan <29433058+kumachan-mis@users.noreply.github.com>
2022-04-16 21:01:01 +0900

    .

--- src/map/map.c
+++ src/map/map.c
@@ -12,11 +12,13 @@ typedef struct _MapCell {
 
 struct _Map {
     MapCell** container;
+    int       size;
     int       capacity;
 };
 
-int      map_calculate_hash(Map* map, char* key);
-int      map_calculate_next_hash(Map* map, char* key, int hash);
+void     map_update_capacity(Map* map, int new_capacity, void delete_value(void* value));
+int      map_calculate_hash(char* key, int capacity);
+int      map_calculate_next_hash(int hash, char* key, int capacity);
 MapCell* new_mapcell(char* key, void* value);
 void     delete_mapcell(MapCell* cell, void delete_value(void* value));
 void     mapcell_markas_deleted(MapCell* cell, void delete_value(void* value));
@@ -24,9 +26,10 @@ void     mapcell_markas_deleted(MapCell* cell, void delete_value(void* value));
 
 Map* new_map() {
     Map* map = malloc(sizeof(Map));
-    int capacity = 1;
+    int size = 0, capacity = 1;
 
     map->container = malloc(capacity * sizeof(MapCell*));
+    map->size = size;
     map->capacity = capacity;
     for (int i = 0; i < capacity; i++) {
         map->container[i] = NULL;
@@ -36,7 +39,8 @@ Map* new_map() {
 
 void delete_map(Map* map, void delete_value(void* value)) {
     for (int i = 0; i < map->capacity; i++) {
-        delete_mapcell(map->container[i], delete_value);
+        MapCell* cell = map->container[i];
+        if (cell != NULL) delete_mapcell(cell, delete_value);
     }
     free(map->container);
     free(map);
@@ -47,70 +51,115 @@ void* map_get(Map* map, char* key) {
 }
 
 void* map_get_with_default(Map* map, char* key,  void* default_value) {
-    int hash = map_calculate_hash(map, key);
+    int hash = map_calculate_hash(key, map->capacity);
     MapCell* cell = map->container[hash];
+
+    int found = 0;
     while (cell != NULL){
         if (!cell->deleted && strcmp(cell->key, key) == 0) {
-            return cell->value;
+           found = 1;
+           break;
         }
-        hash = map_calculate_next_hash(map, key, hash);
+        hash = map_calculate_next_hash(hash, key, map->capacity);
         cell = map->container[hash];
     }
 
-    return default_value;
+    return found? cell->value : default_value;
 }
 
 void map_set(Map* map, char* key, void* value, void delete_value(void* value)) {
-    int hash = map_calculate_hash(map, key);
+    int hash = map_calculate_hash(key, map->capacity);
     MapCell* cell = map->container[hash];
+
+    int found = 0;
     while (cell != NULL && !cell->deleted){
         if (strcmp(cell->key, key) == 0) {
-            delete_value(cell->value);
-            cell->value = value;
-            return;
+            found = 1;
+            break;
         }
-        hash = map_calculate_next_hash(map, key, hash);
+        hash = map_calculate_next_hash(hash, key, map->capacity);
         cell = map->container[hash];
+    }
+
+    if (found) {
+        delete_value(cell->value);
+        cell->value = value;
+        return;
     }
 
     if (cell != NULL) delete_mapcell(cell, delete_value);
     map->container[hash] = new_mapcell(key, value);
+    map->size++;
 
-    int new_capacity = 2 * (map->capacity + 1) - 1;
-    map->container = realloc(map->container, new_capacity * sizeof(MapCell*));
-    for (int i = map->capacity + 1; i < new_capacity; i++) {
-        map->container[i] = NULL;
+    int std_capacity = 2 * (map->size + 1) - 1;
+    if (2 * map->capacity <= std_capacity) {
+        map_update_capacity(map, std_capacity, delete_value);
     }
-    map->capacity = new_capacity;
 }
 
 void map_erase(Map* map, char* key, void delete_value(void* value)) {
-    int hash = map_calculate_hash(map, key);
+    int hash = map_calculate_hash(key, map->capacity);
     MapCell* cell = map->container[hash];
+
+    int found = 0;
     while (cell != NULL){
         if (!cell->deleted && strcmp(cell->key, key) == 0) {
-            mapcell_markas_deleted(cell, delete_value);
-            return;
+           found = 1;
+           break;
         }
-        hash = map_calculate_next_hash(map, key, hash);
+        hash = map_calculate_next_hash(hash, key, map->capacity);
         cell = map->container[hash];
+    }
+
+    if (!found) return;
+
+    mapcell_markas_deleted(cell, delete_value);
+    map->size--;
+
+    int std_capacity = 2 * (map->size + 1) - 1;
+    if (2 * std_capacity < map->capacity) {
+        map_update_capacity(map, std_capacity, delete_value);
     }
 }
 
-int map_calculate_hash(Map* map, char* key) {
-    int a = 154303, b = 628811;
+void map_update_capacity(Map* map, int new_capacity, void delete_value(void* value)) {
+    MapCell** new_container = malloc(new_capacity * sizeof(MapCell*));
+    for (int i = 0; i < new_capacity; i++) new_container[i] = NULL;
+
+    for (int i = 0; i < map->capacity; i++) {
+        MapCell* cell = map->container[i];
+        if (cell == NULL || cell->deleted) {
+            if (cell != NULL) delete_mapcell(cell, delete_value);
+            continue;
+        }
+
+        int rehash = map_calculate_hash(cell->key, new_capacity);
+        while (new_container[rehash] != NULL) {
+            rehash = map_calculate_next_hash(rehash, cell->key, new_capacity);
+        }
+        new_container[rehash] = cell;
+    }
+
+    free(map->container);
+    map->container = new_container;
+    map->capacity = new_capacity;
+}
+
+int map_calculate_hash(char* key, int capacity) {
+    int a = 151, b = 587;
     int hash = 0;
 
     char* key_ptr = key;
     while (*key_ptr != '\0') {
         hash = (*key_ptr * a + hash) % b;
+        key_ptr++;
     }
 
-    return hash % map->capacity;
+    return hash % capacity;
 }
 
-int map_calculate_next_hash(Map* map, char* key, int hash) {
-    return (hash + 1) % map->capacity;
+int map_calculate_next_hash(int hash, char* key, int capacity) {
+    return (hash + 1) % capacity;
 }
 
 MapCell* new_mapcell(char* key, void* value) {
