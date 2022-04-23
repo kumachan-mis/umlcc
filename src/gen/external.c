60febdcd83b30cc0a97c1a2fba6244f6a76d145a
Kumachan <29433058+kumachan-mis@users.noreply.github.com>
2022-04-23 22:09:30 +0900

    .

--- src/gen/external.c
+++ src/gen/external.c
@@ -23,11 +23,9 @@ Vector* gen_translation_unit(Codegen* codegen) {
 Vector* gen_function_definition(Codegen* codegen) {
     Vector* codes = new_vector();
     Srt* srt = codegen->_srt;
-    SymbolTable* table = codegen->_table;
 
     Srt* func_decl = vector_at(srt->children, 0);
     codegen->_srt = func_decl;
-    symboltable_define_symbol(table, func_decl->ident_name, ctype_copy(func_decl->ctype));
 
     Srt* func_body = vector_at(srt->children, 1);
     codegen->_table = new_symboltable();
@@ -45,7 +43,7 @@ Vector* gen_function_definition(Codegen* codegen) {
     append_code(codes, "_%s:\n", func_decl->ident_name);
     append_code(codes, "    pushq  %%rbp\n");
     append_code(codes, "    movq  %%rsp, %%rbp\n");
-    append_code(codes, "    subq  $%d, %%rsp\n", table->_memory_offset);
+    append_code(codes, "    subq  $%d, %%rsp\n", codegen->_table->_memory_offset);
 
     vector_extend(codes, func_codes);
     delete_vector(func_codes, free);
--- src/main.c
+++ src/main.c
@@ -19,33 +19,26 @@ int main(int argc, char *argv[]) {
     Lexer* lexer = new_lexer(src);
     Vector* tokens = lexer_read_tokens(lexer);
     delete_lexer(lexer);
+    fprintf(stderr, "lexer\n");
 
     Parser* parser = new_parser(tokens);
     Ast* ast = parser_create_ast(parser);
     delete_parser(parser);
+    fprintf(stderr, "parser\n");
 
     Resolver* resolver = new_resolver(ast);
     Srt* srt = resolver_resolve_semantics(resolver);
     delete_resolver(resolver);
+    fprintf(stderr, "resolver\n");
 
     Codegen* codegen = new_codegen(srt);
     Vector* codes = codegen_generate_code(codegen);
-
-    fprintf(dest, "    .text\n");
-    fprintf(dest, "    .global _main\n");
-    fprintf(dest, "_main:\n");
-    fprintf(dest, "    pushq  %%rbp\n");
-    fprintf(dest, "    movq  %%rsp, %%rbp\n");
-    fprintf(dest, "    subq  $%d, %%rsp\n", codegen->_table->_memory_offset);
+    fprintf(stderr, "codegen\n");
 
     int codes_len = vector_size(codes);
     for (int i = 0; i < codes_len; i++) {
         fprintf(dest, "%s", (char*)vector_at(codes, i));
     }
-
-    fprintf(dest, "    addq  $%d, %%rsp\n", codegen->_table->_memory_offset);
-    fprintf(dest, "    popq  %%rbp\n");
-    fprintf(dest, "    ret\n");
 
     delete_codegen(codegen);
     delete_vector(codes, free);
