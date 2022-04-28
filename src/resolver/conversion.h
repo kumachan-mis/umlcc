84ed22f77cafe216bc7f164915c43f1c47c3ddb3
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2022-04-28 20:59:46 +0900

    .

---
+++ src/resolver/conversion.h
@@ -0,0 +1,9 @@
+#ifndef _UMLCC_RESOLVER_CONVERSION_H_
+#define _UMLCC_RESOLVER_CONVERSION_H_
+
+#include "../srt/srt.h"
+
+
+Srt* convert_function_to_ptr(Srt* srt);
+
+#endif
--- src/resolver/expression.c
+++ src/resolver/expression.c
@@ -1,4 +1,5 @@
 #include "./expression.h"
+#include "./conversion.h"
 #include "../common/common.h"
 
 #include <stdio.h>
@@ -97,21 +98,37 @@ Srt* resolve_multiplicative_expr(Resolver* resolver) {
 Srt* resolve_postfix_expr(Resolver* resolver) {
     Ast* ast = resolver->_ast;
 
-    resolver->_ast = vector_at(ast->children, 0);
-    Srt* child = resolve_expr(resolver);
-
-    CType* ctype = ctype_copy(child->ctype);
-    resolver->_ast = ast;
-
     switch (ast->type) {
-        case AST_CALL_EXPR:
-            child = new_ctyped_srt(SRT_ADDR_EXPR, new_pointer_ctype(ctype), 1, child);
-            // function -> pointer to function. TODO: implicit conversion as a module.
-            return new_ctyped_srt(SRT_CALL_EXPR, ctype_copy(ctype->function->return_type), 1, child);
+        case AST_CALL_EXPR: {
+            resolver->_ast = vector_at(ast->children, 0);
+            Srt* lhs_srt = convert_function_to_ptr(resolve_expr(resolver));
+            // TODO: type conversion is always performed, not only in function calls
+        
+            resolver->_ast = vector_at(ast->children, 1);
+            Srt* rhs_srt = resolve_argument_expr_list(resolver);
+            
+            CType* ctype = ctype_copy(ctype->function->return_type);
+            resolver->_ast = ast;
+            return new_ctyped_srt(SRT_CALL_EXPR, ctype, 2, lhs_srt, rhs_srt);
+        }
         default:
             fprintf(stderr, "Error: unexpected ast type %d\n", ast->type);
             exit(1);
     }
+}
+
+Srt* resolve_argument_expr_list(Resolver* resolver) {
+    Srt* srt = new_srt(SRT_ARG_LIST, 0);
+    Ast* ast = resolver->_ast;
+
+    int num_children = vector_size(ast->children);
+    for (int i = 0; i < num_children; i++) {
+        resolver->_ast = vector_at(ast->children, i);
+        vector_push(srt->children, resolve_expr(resolver));
+    }
+
+    resolver->_ast = ast;
+    return srt;
 }
 
 Srt* resolve_primary_expr(Resolver* resolver) {
