9bbced21a213a560eb9c642e9cd116876c9857ae
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2022-04-30 11:05:45 +0000

    .

---
+++ scripts/testlib.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int put_int(int x) {
+    printf("%d\n", x);
+}
---
+++ test-fixtures/arithmetic/input.c
@@ -0,0 +1,9 @@
+int put_int(int x);
+
+int main() {
+    put_int(1 + 2);
+    put_int(3 - 8);
+    put_int(4 * 2);
+    put_int(17 / 4);
+    0;
+}
