34392c8500772b578c1e49d3ff506aeb8816e055
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2022-05-03 23:38:27 +0000

    .

--- sample/functionall.c
+++ sample/functionall.c
@@ -1,3 +1,5 @@
+int create_param();
+
 void func_param_function(void function(void)) {
     function();
 }
@@ -17,9 +19,15 @@ int eight_param_function(int a, int b, int c, int d, int e, int f, int g, int h)
     return a + b + c + d + e + f + g + h;
 }
 
-int main(void) {
+void usual_function_call() {
     func_param_function(zero_param_function);
     six_param_function(1, 2, 3, 4, 5, 6);
     seven_param_function(1, 2, 3, 4, 5, 6, 7);
     eight_param_function(1, 2, 3, 4, 5, 6, 7, 8);
+}
+
+void reg_saving_function_call() {
+    six_param_function(1, 2, 3, 4, create_param(), 6);
+    six_param_function(1, 2, 3, create_param(), create_param(), 6);
+    six_param_function(1, 2, 3, create_param(), create_param(), create_param());
 }
