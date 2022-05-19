ea338797f1048a32839a49cb8b556de704ef9996
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2022-05-19 00:38:45 +0000

    .

--- src/liveseq/liveseq.c
+++ src/liveseq/liveseq.c
@@ -7,20 +7,7 @@ Liveseq* new_liveseq(Vector* livenesses) {
     Liveseq* liveseq = malloc(sizeof(Liveseq));
     liveseq->livenesses = livenesses;
     liveseq->index = 0;
-
-    int livenesses_len = vector_size(liveseq->livenesses);
-    if (livenesses_len == 0) {
-        liveseq->liveness_index = -1;
-        return liveseq;
-    }
-
-    Liveness* first_liveness = vector_at(liveseq->livenesses, 0);
-    if (liveseq->index < first_liveness->first_def_index) {
-        liveseq->liveness_index = -1;
-        return liveseq;
-    }
-
-    liveseq->liveness_index = 0;
+    liveseq->liveness_index = -1;
     return liveseq;
 }
 
@@ -30,22 +17,22 @@ void delete_liveseq(Liveseq* liveseq) {
 }
 
 int liveseq_need_evacuation(Liveseq* liveseq) {
-    int livenesses_len = vector_size(liveseq->livenesses);
-    if (livenesses_len == 0 || liveseq->liveness_index < 0 ||
-        liveseq->liveness_index >= livenesses_len) {
-        return 0;
-    }
+    // following registers don't need to evacuate:
+    // - registers to be first-defined at current index
+    // - registers which are last-used at current index
 
     int index = liveseq->index;
-    Liveness* liveness = vector_at(liveseq->livenesses, liveseq->liveness_index);
-    int need_evacuation = liveness->first_def_index <= index && index < liveness->last_use_index;
 
-    if (liveseq->liveness_index < livenesses_len - 1) {
-        Liveness* next_liveness = vector_at(liveseq->livenesses, liveseq->liveness_index + 1);
-        need_evacuation = need_evacuation || next_liveness->first_def_index <= index;
+    int livenesses_len = vector_size(liveseq->livenesses);
+    if (livenesses_len == 0 || liveseq->liveness_index >= livenesses_len) return 0;
+
+    if (liveseq->liveness_index < 0) {
+        Liveness* first_liveness = vector_at(liveseq->livenesses, 0);
+        return index > first_liveness->first_def_index;
     }
 
-    return need_evacuation;
+    Liveness* liveness = vector_at(liveseq->livenesses, liveseq->liveness_index);
+    return liveness->first_def_index = index && index < liveness->last_use_index;
 }
 
 void liveseq_goto_next(Liveseq* liveseq) {
@@ -63,7 +50,7 @@ void liveseq_goto_next(Liveseq* liveseq) {
 
     Liveness* liveness = vector_at(liveseq->livenesses, liveseq->liveness_index);
     if (liveseq->liveness_index == livenesses_len - 1) {
-        if (index >= liveness->last_use_index) liveseq->liveness_index++;
+        if (index >= liveness->last_use_index) liveseq->liveness_index = livenesses_len;
         return;
     }
 
