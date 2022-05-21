163c84a32a5030b22463165cf53e9a14b4437244
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2022-05-21 00:53:53 +0000

    .

--- src/x64/util.c
+++ src/x64/util.c
@@ -31,7 +31,7 @@ Set* create_alive_regs_set(Vector* liveseqs) {
     int liveseqs_len = vector_size(liveseqs);
     for (int reg_id = 0; reg_id < liveseqs_len; reg_id++) {
         Liveseq* liveseq = vector_at(liveseqs, reg_id);
-        if (liveseq_is_alive(liveseq)) { set_add(alive_regs_set, new_integer(reg_id)); }
+        if (liveseq_is_alive(liveseq)) set_add(alive_regs_set, new_integer(reg_id));
     }
     return alive_regs_set;
 }
