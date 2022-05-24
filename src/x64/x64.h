31515d8884692fb2215614dd96fd546197eb4fd4
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2022-05-24 04:28:55 +0000

    .

--- src/x64/x64.h
+++ src/x64/x64.h
@@ -12,7 +12,7 @@ typedef struct X64gen {
 } X64gen;
 
 X64gen* new_x64gen(Vector* immcs, Vector* liveseqs);
-void delete_x64gen(X64gen* x64gen);
 Vector* x64gen_generate_x64code(X64gen* x64gen);
+void delete_x64gen(X64gen* x64gen);
 
 #endif
