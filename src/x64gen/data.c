3bdfdea6a19be40fb7c9f9e2e141067deec8b65d
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2022-06-11 22:39:11 +0000

    .

--- src/x64gen/data.c
+++ src/x64gen/data.c
@@ -8,19 +8,19 @@ Vector* gen_data_x64code(X64gen* x64gen) {
 
     switch (immc->data->type) {
         case IMMC_DATA_BYTE:
-            append_code(codes, "    .byte %d\n", immc->data->value);
+            append_code(codes, "\t.byte %d\n", immc->data->value);
             break;
         case IMMC_DATA_WORD:
-            append_code(codes, "    .word %d\n", immc->data->value);
+            append_code(codes, "\t.word %d\n", immc->data->value);
             break;
         case IMMC_DATA_LONG:
-            append_code(codes, "    .long %d\n", immc->data->value);
+            append_code(codes, "\t.long %d\n", immc->data->value);
             break;
         case IMMC_DATA_QUAD:
-            append_code(codes, "    .quad %d\n", immc->data->value);
+            append_code(codes, "\t.quad %d\n", immc->data->value);
             break;
         case IMMC_DATA_ZERO:
-            append_code(codes, "    .zero %d\n", immc->data->value);
+            append_code(codes, "\t.zero %d\n", immc->data->value);
             break;
     }
 
