bc95411ea2cc8d490e4dc05807647d8f3bc2b7a9
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2022-07-02 04:07:13 +0000

    more test

--- test-fixtures/typedef/main.c
+++ test-fixtures/typedef/main.c
@@ -2,6 +2,8 @@ typedef int test_int;
 typedef int* test_intptr;
 
 test_int put_int(test_int x);
+test_int put_str(char* s);
+test_int put_blank_line();
 
 int main() {
     test_int a, b;
@@ -17,6 +19,23 @@ int main() {
     put_int(*q);
     put_int(p == &a);
     put_int(q == &b);
+    put_blank_line();
+
+    char typedef test_char;
+    test_char x = 20, y = 40, z = 50, w = 10;
+    put_int((x * y + z) / w);
+    {
+        char typedef* string;
+        string s = "ptr string";
+        put_str(s);
+    }
+
+    {
+        char typedef string[14];
+        string s = "array string";
+        put_str(s);
+        put_int(s[13]);
+    }
 
     return 0;
 }
