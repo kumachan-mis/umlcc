ce59b1b22b255f3e75aed6d3bf7fe4da2ea907c3
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2022-07-30 01:09:46 +0000

    testlib

--- tests/testlib/testlib.h
+++ tests/testlib/testlib.h
@@ -2,9 +2,19 @@
 #define UMLCC_TESTLIB_H
 
 #include "../../src/ast/ast.h"
+#include "../../src/ctoken/ctoken.h"
+#include "../../src/literal/iliteral.h"
+#include "../../src/literal/sliteral.h"
+#include "../../src/srt/srt.h"
 #include "../../src/vector/vector.h"
 
 int testlib_ctokens_equals(Vector* actual, Vector* expected);
+int testlib_ctoken_equals(CToken* actual, CToken* expected);
 int testlib_ast_equals(Ast* actual, Ast* expected);
+int testlib_srt_equals(Srt* actual, Srt* expected);
+int testlib_dtype_equals(Dtype* actual, Dtype* expected);
+int testlib_iliteral_equals(IntegerLiteral* actual, IntegerLiteral* expected);
+int testlib_sliteral_equals(StringLiteral* actual, StringLiteral* expected);
+int testlib_string_equals(char* actual, char* expected);
 
 #endif
