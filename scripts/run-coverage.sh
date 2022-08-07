64f45ed5711b3e30a895418479138ad5f13fa3d6
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2022-08-07 03:43:25 +0000

    converage config file

--- scripts/run-coverage.sh
+++ scripts/run-coverage.sh
@@ -4,10 +4,13 @@ BASE_DIR=$(cd $(dirname $0)/.. && pwd)
 
 COBJ_DIR=${BASE_DIR}/build/src/cobject
 COV_DIR=${BASE_DIR}/cov
+CONFIG_FILE=${BASE_DIR}/.coverage
 
-RED_THRESHOLD=50
-YELLOW_THRESHOLD=80
-GREEN_THRESHOLD=100
+CONFIG=$(cat ${CONFIG_FILE})
+GCOV_ARGS=$(echo ${CONFIG} | jq -r ".gcovArgs[]")
+RED_THRESHOLD=$(echo ${CONFIG} | jq ".colorThreshold.red")
+YELLOW_THRESHOLD=$(echo ${CONFIG} | jq ".colorThreshold.yellow")
+GREEN_THRESHOLD=$(echo ${CONFIG} | jq ".colorThreshold.green")
 
 RED="\e[1;31m"
 GREEN="\e[1;32m"
@@ -17,6 +20,7 @@ END="\e[0m"
 
 print_gcov () {
     gcov_summary=$1
+
     while read -r line; do
         if ! [[ ${line} =~ ^(.*):([0-9]{1,3})\.([0-9]{2})%(.*)$ ]]; then
             echo "${line}"
@@ -50,6 +54,6 @@ do
     cov_file=$(basename ${cobj_file} | sed "s|.o$|.c.gcov|")
 
     mkdir -p ${cov_dir}
-    print_gcov "$(gcov -r ${cobj_file})"
+    print_gcov "$(gcov ${GCOV_ARGS} ${cobj_file})"
     mv ${cov_file} ${cov_dir}
 done
