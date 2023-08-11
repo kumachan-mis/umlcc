e52490444b530577baa4f96addade6c01a119334
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2023-08-11 04:00:44 +0000

    quad arithmetic sample

--- sample/arithmetic.c
+++ sample/arithmetic.c
@@ -1,34 +1,69 @@
-int add(void) {
+int addl(void) {
     int a = 14;
     int b = 3;
     int c = a + b;
     return c;
 }
 
-int sub(void) {
+int subl(void) {
     int a = 14;
     int b = 3;
     int c = a - b;
     return c;
 }
 
-int mul(void) {
+int mull(void) {
     int a = 14;
     int b = 3;
     int c = a * b;
     return c;
 }
 
-int div(void) {
+int divl(void) {
     int a = 14;
     int b = 3;
     int c = a / b;
     return c;
 }
 
-int mod(void) {
+int modl(void) {
     int a = 14;
     int b = 3;
     int c = a % b;
+    return c;
+}
+
+long long addq(void) {
+    long long a = 14LL;
+    long long b = 3LL;
+    long long c = a + b;
+    return c;
+}
+
+long long subq(void) {
+    long long a = 14LL;
+    long long b = 3LL;
+    long long c = a - b;
+    return c;
+}
+
+long long mulq(void) {
+    long long a = 14LL;
+    long long b = 3LL;
+    long long c = a * b;
+    return c;
+}
+
+long long divq(void) {
+    long long a = 14LL;
+    long long b = 3LL;
+    long long c = a / b;
+    return c;
+}
+
+long long modq(void) {
+    long long a = 14LL;
+    long long b = 3LL;
+    long long c = a % b;
     return c;
 }
