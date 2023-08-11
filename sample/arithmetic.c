22b59087e64b13e65107f090ca98914962891652
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2023-08-11 10:32:35 +0000

    add unsigned sample

--- sample/arithmetic.c
+++ sample/arithmetic.c
@@ -33,6 +33,41 @@ int modl(void) {
     return c;
 }
 
+unsigned uaddl(void) {
+    unsigned a = 14;
+    unsigned b = 3;
+    unsigned c = a + b;
+    return c;
+}
+
+unsigned usubl(void) {
+    unsigned a = 14;
+    unsigned b = 3;
+    unsigned c = a - b;
+    return c;
+}
+
+unsigned umull(void) {
+    unsigned a = 14;
+    unsigned b = 3;
+    unsigned c = a * b;
+    return c;
+}
+
+unsigned udivl(void) {
+    unsigned a = 14;
+    unsigned b = 3;
+    unsigned c = a / b;
+    return c;
+}
+
+unsigned muodl(void) {
+    unsigned a = 14;
+    unsigned b = 3;
+    unsigned c = a % b;
+    return c;
+}
+
 long long addq(void) {
     long long a = 14LL;
     long long b = 3LL;
@@ -65,5 +100,40 @@ long long modq(void) {
     long long a = 14LL;
     long long b = 3LL;
     long long c = a % b;
+    return c;
+}
+
+unsigned long long uaddq(void) {
+    unsigned long long a = 14;
+    unsigned long long b = 3;
+    unsigned long long c = a + b;
+    return c;
+}
+
+unsigned long long usubq(void) {
+    unsigned long long a = 14;
+    unsigned long long b = 3;
+    unsigned long long c = a - b;
+    return c;
+}
+
+unsigned long long umulq(void) {
+    unsigned long long a = 14;
+    unsigned long long b = 3;
+    unsigned long long c = a * b;
+    return c;
+}
+
+unsigned long long udivq(void) {
+    unsigned long long a = 14;
+    unsigned long long b = 3;
+    unsigned long long c = a / b;
+    return c;
+}
+
+unsigned long long umodq(void) {
+    unsigned long long a = 14;
+    unsigned long long b = 3;
+    unsigned long long c = a % b;
     return c;
 }
