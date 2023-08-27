9a261a76690185bea0fb522d629d088d1b07c0c9
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2023-08-27 10:08:06 +0000

    implement e2etest

--- test-fixtures/comparison/main.c
+++ test-fixtures/comparison/main.c
@@ -1,9 +1,40 @@
 void put_int(int x);
+void put_blank_line(void);
 
-int main(void) {
+void equality(void) {
     put_int(5 == 5);
     put_int(5 == 4);
+    put_blank_line();
+
     put_int(5 != 5);
     put_int(5 != 4);
+    put_blank_line();
+}
+
+void relational(void) {
+    put_int(5 < 6);
+    put_int(5 < 5);
+    put_int(5 < 4);
+    put_blank_line();
+
+    put_int(5 > 6);
+    put_int(5 > 5);
+    put_int(5 > 4);
+    put_blank_line();
+
+    put_int(5 <= 6);
+    put_int(5 <= 5);
+    put_int(5 <= 4);
+    put_blank_line();
+
+    put_int(5 >= 6);
+    put_int(5 >= 5);
+    put_int(5 >= 4);
+    put_blank_line();
+}
+
+int main(void) {
+    equality();
+    relational();
     return 0;
 }
