d1ddfaf574288954656143357cb647bffbbf4d9b
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2023-10-15 05:08:55 +0000

    update sample

--- sample/struct.c
+++ sample/struct.c
@@ -1,3 +1,5 @@
+int put_int(int x);
+
 struct Child {
     char x;
     int y;
@@ -10,7 +12,25 @@ struct Struct {
 };
 
 struct Struct g = {1};
+struct Child gc = {1};
 
 int main(void) {
     struct Struct l = {1};
+    struct Child lc = {1};
+
+    put_int(lc.x);
+    put_int(lc.y);
+
+    put_int(l.a.x);
+    put_int(l.a.y);
+    put_int(l.b.x);
+    put_int(l.b.y);
+
+    put_int(gc.x);
+    put_int(gc.y);
+
+    put_int(g.a.x);
+    put_int(g.a.y);
+    put_int(g.b.x);
+    put_int(g.b.y);
 }
