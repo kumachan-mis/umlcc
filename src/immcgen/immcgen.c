c2c5372efe79b99be57dcbeb12ca803e9ab0b2bd
Kumachan <29433058+kumachan-mis@users.noreply.github.com>
2023-11-03 11:42:10 +0900

    refactor: memory access code (#135)

--- src/immcgen/immcgen.c
+++ src/immcgen/immcgen.c
@@ -121,8 +121,10 @@ Vector* immcgen_generate_immcode(Immcgen* immcgen) {
             break;
         case SRT_PADD_EXPR:
         case SRT_PSUB_EXPR:
-        case SRT_PDIFF_EXPR:
             codes = gen_pointer_additive_expr_immcode(immcgen);
+            break;
+        case SRT_PDIFF_EXPR:
+            codes = gen_pointer_diff_expr_immcode(immcgen);
             break;
         case SRT_MUL_EXPR:
         case SRT_DIV_EXPR:
